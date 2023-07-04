419fd48e3e2aad58be690e61a11c6918078bea11
ReverseSerj <sej.petrov@gmail.com>
2023-07-04 11:18:29 +0300

    fail fixes

--- routes/cards.js
+++ routes/cards.js
@@ -8,5 +8,4 @@ router.post('/cards', createCard);
 router.delete('/cards/:cardId', deleteCard);
 router.put('/cards/:cardId/likes', likeCard);
 router.delete('/cards/:cardId/likes', dislikeCard);
-
 module.exports = router;
