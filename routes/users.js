d9858acbb64dcf5bc5a7439a1334fc84604da69a
ReverseSerj <sej.petrov@gmail.com>
2023-07-13 01:50:25 +0300

    Revert "joi"
    This reverts commit 81c51a79e6ddc151f171714384c8570d8c00b279.

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,16 @@ const router = require('express').Router();
 const {
   getUsers, getUser, createUser, updateProfile, updateAvatar, getCurrentUser,
 } = require('../controllers/users');
+
 const {
   getUserValidator, updateProfileValidator, updateAvatarValidator,
 } = require('../validators/validators');
 
 router.get('/users/me', getCurrentUser);
 router.get('/users', getUsers);
-router.get('/users/:userId', getUserValidator, getUser);
+router.get('/users/:userId', getUser, getUserValidator);
 router.post('/users', createUser);
-router.patch('/users/me', updateProfileValidator, updateProfile);
-router.patch('/users/me/avatar', updateAvatarValidator, updateAvatar);
+router.patch('/users/me', updateProfile, updateProfileValidator);
+router.patch('/users/me/avatar', updateAvatar, updateAvatarValidator);
 
 module.exports = router;
