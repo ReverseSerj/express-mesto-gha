82547f8332b2beadac23560b5f3944e19023de8c
ReverseSerj <sej.petrov@gmail.com>
2023-07-12 22:06:01 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -105,7 +105,7 @@ module.exports.updateAvatar = (req, res, next) => {
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
-  return User.findUserByCredentials(email, password)
+  return User.findUserByData(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.send({ token });
