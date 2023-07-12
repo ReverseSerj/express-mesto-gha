a2a24fe20fe51412616ef6d38e47a166f15baca6
ReverseSerj <sej.petrov@gmail.com>
2023-07-13 02:38:31 +0300

    joi

--- validators/validators.js
+++ validators/validators.js
@@ -1,4 +1,5 @@
-const { celebrate, Joi } = require('celebrate');
+const { celebrate } = require('celebrate');
+const Joi = require('joi');
 const { regexURL } = require('../utils/regex');
 
 module.exports.getUserValidator = celebrate({
