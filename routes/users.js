81c51a79e6ddc151f171714384c8570d8c00b279
ReverseSerj <sej.petrov@gmail.com>
2023-07-13 00:58:24 +0300

    joi

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,15 @@ const router = require('express').Router();
 const {
   getUsers, getUser, createUser, updateProfile, updateAvatar, getCurrentUser,
 } = require('../controllers/users');
-
 const {
   getUserValidator, updateProfileValidator, updateAvatarValidator,
 } = require('../validators/validators');
 
 router.get('/users/me', getCurrentUser);
 router.get('/users', getUsers);
-router.get('/users/:userId', getUser, getUserValidator);
+router.get('/users/:userId', getUserValidator, getUser);
 router.post('/users', createUser);
-router.patch('/users/me', updateProfile, updateProfileValidator);
-router.patch('/users/me/avatar', updateAvatar, updateAvatarValidator);
+router.patch('/users/me', updateProfileValidator, updateProfile);
+router.patch('/users/me/avatar', updateAvatarValidator, updateAvatar);
 
 module.exports = router;
